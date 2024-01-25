2b22bd140f18be705282a68cc4a52b9b0a2dd858
abhi-s-03 <abhisheks3002@gmail.com>
2024-01-25 18:41:43 +0530

    Add mobile navigation menu

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,11 @@
 import "./navbar.css";
 import { DiCssdeck } from "react-icons/di";
+import { FaBars } from "react-icons/fa";
 import { Bio } from "../../data/data.js";
+import { useState } from "react";
 
 export default function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <>
       <div className="navbar">
@@ -10,6 +13,13 @@ export default function Navbar() {
           <div className="logo">
             <DiCssdeck size="3rem" id="white" />
             <span id="navbar-logo">Portfolio</span>
+          </div>
+          <div className="mobileicon">
+            <FaBars
+              onClick={() => {
+                setIsOpen(!isOpen);
+              }}
+            />
           </div>
           <div className="navbar-items">
             <a href="/">About</a>
@@ -21,6 +31,51 @@ export default function Navbar() {
           <div className="navbar-resume">
             <a href={Bio.resume}>Resume</a>
           </div>
+          {isOpen && (
+            <div className="mobile-navbar-items">
+              <a
+                href="/"
+                onClick={() => {
+                  setIsOpen(!isOpen);
+                }}
+              >
+                About
+              </a>
+              <a
+                href="/"
+                onClick={() => {
+                  setIsOpen(!isOpen);
+                }}
+              >
+                Skills
+              </a>
+              <a
+                href="/"
+                onClick={() => {
+                  setIsOpen(!isOpen);
+                }}
+              >
+                Education
+              </a>
+              <a
+                href="/"
+                onClick={() => {
+                  setIsOpen(!isOpen);
+                }}
+              >
+                Experience
+              </a>
+              <a
+                href="/"
+                onClick={() => {
+                  setIsOpen(!isOpen);
+                }}
+              >
+                Projects
+              </a>
+              <a href={Bio.resume}>Resume</a>
+            </div>
+          )}
         </div>
       </div>
     </>
