696e5ba1d2be1a9f52e6ed1049deb5b60b2d3cf9
abhi-s-03 <abhisheks3002@gmail.com>
2025-11-14 22:12:27 +0530

    version 2.0 fix

--- src/components/education/Edu.jsx
+++ src/components/education/Edu.jsx
@@ -1,13 +1,6 @@
 import "./edu.css";
-import {
-  Timeline,
-  TimelineItem,
-  TimelineSeparator,
-  TimelineConnector,
-  TimelineContent,
-  TimelineDot,
-} from "@mui/lab";
 import { education } from "../../data/data.js";
+
 export default function Edu() {
   return (
     <>
@@ -15,43 +8,24 @@ export default function Edu() {
         <div className="wrapper">
           <div className="edu-title">Education</div>
           <div className="edu-content">
-            <Timeline>
-              {education.map((item, index) => (
-                <TimelineItem key={index}>
-                  <TimelineContent>
-                    <div
-                      className="edu-card"
-                      style={{ border: "0.1px solid #854ce6" }}
-                    >
-                      <div className="educard-top">
-                        <div className="educard-img">
-                          <img src={item.img} alt="" />
-                        </div>
-                        <div className="educard-body">
-                          <div className="educard-name">{item.school}</div>
-                          <div className="educard-degree">{item.degree}</div>
-                          <div className="educard-date">{item.date}</div>
-                        </div>
-                      </div>
-                      <div className="educard-grade">{item.grade}</div>
-                      <div className="educard-desc">
-                        <span>{item.desc}</span>
-                      </div>
-                    </div>
-                  </TimelineContent>
-                  <TimelineSeparator>
-                    <TimelineDot
-                      variant="outlined"
-                      color="secondary"
-                      style={{ marginRight: "20px" }}
-                    />
-                    <TimelineConnector
-                      style={{ background: "#854CE6", marginRight: "20px" }}
-                    />
-                  </TimelineSeparator>
-                </TimelineItem>
-              ))}
-            </Timeline>
+            {education.map((item, index) => (
+              <div key={index} className="edu-card">
+                <div className="educard-top">
+                  <div className="educard-img">
+                    <img src={item.img} alt="" />
+                  </div>
+                  <div className="educard-body">
+                    <div className="educard-name">{item.school}</div>
+                    <div className="educard-degree">{item.degree}</div>
+                    <div className="educard-date">{item.date}</div>
+                  </div>
+                </div>
+                <div className="educard-grade">{item.grade}</div>
+                <div className="educard-desc">
+                  <span>{item.desc}</span>
+                </div>
+              </div>
+            ))}
           </div>
         </div>
       </div>
